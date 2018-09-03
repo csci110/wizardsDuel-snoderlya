ac3c596f3022a798f89e56960a893168195987d1
Allison Snoderly <snoderlya@dewv.edu>
2018-09-03 16:49:38 +0000

    Updates via sync script

--- wizardsDuel.js
+++ wizardsDuel.js
@@ -1,1 +1,17 @@
+import {game, Sprite} from "./sgc/sgc.js";
 
+game.setBackground("floor.png");
+
+class PlayerWizard extends Sprite{
+    constructor() {
+        super();
+        this.name = "Marcus the Wizard";
+        this.setImage(marcusSheet.png);
+        this.height =48;
+        this.width = 48;
+        this.x = this.width;
+        this.y = this.height;
+            }
+}
+
+let marcus = new PlayerWizard;
