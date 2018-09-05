f72da251f8d0bd98080a706fa414aac5cf7806cd
Allison Snoderly <snoderlya@dewv.edu>
2018-09-05 16:34:21 +0000

    Updates via sync script

---
+++ loader.js
@@ -0,0 +1,4 @@
+import {game} from "./sgc/sgc.js";
+
+game.preloadImage("marcusSheet.png", 48, 48);
+game.preloadImage("floor.png");
--- wizardsDuel.js
+++ wizardsDuel.js
@@ -1,17 +1,32 @@
-import {game, Sprite} from "./sgc/sgc.js";
+import { game, Sprite } from "./sgc/sgc.js";
 
 game.setBackground("floor.png");
 
-class PlayerWizard extends Sprite{
+class PlayerWizard extends Sprite {
     constructor() {
         super();
         this.name = "Marcus the Wizard";
-        this.setImage(marcusSheet.png);
-        this.height =48;
+        this.setImage("marcusSheet.png");
+        this.height = 48;
         this.width = 48;
         this.x = this.width;
-        this.y = this.height;
-            }
+        this.y = this.y;
+        this.defineAnimation("down",6, 8);
+        this.playAnimation("down");
+        this.speedWhenWalking = 100;
+        this.defineAnimation("up", 6, 8)
+        this.playAnimation("up")
+    }
+    handleDownArrowKey() {
+            this.playAnimation("down");
+            this.speed =this.speedWhenWalking;
+            this.angle = 270;
+    }
+    handleUpArrowKey() {
+        this.playAnimation("up");
+        this.speed = this.speedWhenWalking;
+        this.angle = 90;
+    }
 }
 
 let marcus = new PlayerWizard;
